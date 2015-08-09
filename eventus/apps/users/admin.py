d994f575a535a74075e3a51345f62ee3f0510985
Wilzon Mariño Bueno <wimarbueno@gmail.com>
2015-08-09 12:20:59 -0500

    exportar a excel

--- eventus/apps/users/admin.py
+++ eventus/apps/users/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from .models import User
+from .actions import export_as_excel
 
 # Register your models here.
-admin.site.register(User)
+#admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'first_name', 'last_name', 'email')
+    search_fields = ('username', 'email')
+    list_filter = ('is_superuser',)
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions')
+    actions = [export_as_excel]
+
+    fieldsets = (
+        ('User', {'fields' : ('username', 'password')}),
+        ('Persona Info', {'fields' : (
+                        'first_name',
+                        'last_name',
+                        'email',
+                        'avatar',
+                    )}),
+        ('Permissions', {'fields' : (
+                        'is_active',
+                        'is_staff',
+                        'is_superuser',
+                        'groups',
+                        'user_permissions',
+                    )}),
+    )
